4180c1e40f97069275a3c4a214d1fa58ceb14161
Nide17 <nidedrogba@gmail.com>
2023-09-12 19:59:56 +0200

    ssl bug

--- auth-service/config/db.js
+++ auth-service/config/db.js
@@ -4,4 +4,11 @@ require('dotenv').config();
 
 // CONNECTION TO POSTGRESQL DATABASE USING SEQUELIZE
 module.exports = new Sequelize(
-    `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+    `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+}
+)
